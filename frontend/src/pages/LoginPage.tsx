75a6a0b066e31c02513e55442081553d283af235
VladiMedvedi <vladislav.medvedi@lynxspa.com>
2025-02-13 00:40:13 +0100

    login/signup pages added

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,44 @@
+import { Form, Input, Button } from 'antd'
+import { Link, useNavigate } from 'react-router-dom'
 const LoginPage = () => {
+    const [form] = Form.useForm()
+    const navigate = useNavigate()
+    const onFinish = (values: { username: string, password: string }) => {
+        console.log(values)
+        navigate('/dashboard')
+    }
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="flex justify-center items-center h-full">
+            <Form
+                layout="vertical"
+                onFinish={onFinish}
+                form={form}
+                className="w-[90%] max-w-[400px] mx-auto bg-white rounded-lg border border-gray-200 p-6! shadow-md "
+            >
+                <Form.Item
+                    name="email"
+                    label="Email"
+                    rules={[
+                        { required: true, message: 'Please input your email!' },
+                        { type: 'email', message: 'Please enter a valid email address!' }
+                    ]}
+                >
+                    <Input type="email" size="large" />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    label="Password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password size="large" />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" block size="large" htmlType="submit">Login</Button>
+                </Form.Item>
+                <div className="text-center text-gray-500">
+                    Don't have an account? <Link to="/signup">Sign up</Link>
+                </div>
+            </Form>
         </div>
     )
 }
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,7 +1,52 @@
+import { Button, Form, Input } from 'antd'
+import { Link, useNavigate } from 'react-router-dom'
+
 const SignUpPage = () => {
+    const [form] = Form.useForm()
+    const navigate = useNavigate()
+    const onFinish = (values: { username: string, password: string }) => {
+        console.log(values)
+        navigate('/dashboard')
+    }
     return (
-        <div>
-            <h1>SignUp</h1>
+        <div className="flex justify-center items-center h-full">
+            <Form
+                layout="vertical"
+                onFinish={onFinish}
+                form={form}
+                className="w-[90%] max-w-[400px] mx-auto bg-white rounded-lg border border-gray-200 p-6! shadow-md "
+            >
+                <Form.Item
+                    name="name"
+                    label="Name"
+                    rules={[
+                        { required: true, message: 'Please input your name!' },
+                    ]}
+                >
+                    <Input size="large" />
+
+                </Form.Item>
+                <Form.Item
+                    name="email"
+                    label="Email"
+                    rules={[{ required: true, message: 'Please input your email!' }]}
+                >
+                    <Input size="large" />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    label="Password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password size="large" />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" block size="large" htmlType="submit">Sign Up</Button>
+                </Form.Item>
+                <div className="text-center text-gray-500">
+                    Already have an account? <Link to="/login">Login</Link>
+                </div>
+            </Form>
         </div>
     )
 }
