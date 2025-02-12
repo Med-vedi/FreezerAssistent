f794b29baa1a3273107736e3952fd1d147c4d2f8
VladiMedvedi <vladislav.medvedi@lynxspa.com>
2025-02-13 00:27:31 +0100

    routing added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
 import './App.css'
 import MainLayout from './layout/MainLayout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import Dashboard from './pages/Dashboard'
+
 function App() {
   return (
-    <MainLayout>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </MainLayout>
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={
+            <div>
+              <h1>Hello World</h1>
+            </div>
+          } />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
   )
 }
 
