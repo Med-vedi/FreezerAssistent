0a4c218e3dcb293513945d408839f06a4b5e4893
VladiMedvedi <vladislav.medvedi@lynxspa.com>
2025-02-22 20:52:49 +0100

    Fix Vercel deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
