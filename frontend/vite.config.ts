bd0987680a585ff25db68dbf7e3c902be3c29b17
VladiMedvedi <vladislav.medvedi@lynxspa.com>
2025-02-24 22:15:27 +0100

    vercel update 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
+  },
+  server: {
+    // historyApiFallback: true, // Ensures proper routing in dev mode
   },
   plugins: [tailwindcss(), react()],
   resolve: {
